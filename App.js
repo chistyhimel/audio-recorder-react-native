43ebca61c0a214265a2f814647042f578f26f395
chistyhimel <fazle.chisty@gmail.com>
2024-03-18 23:35:07 +0600

    update

---
+++ App.js
@@ -0,0 +1,30 @@
+import {StyleSheet, Text} from 'react-native';
+
+function App() {
+  return (
+    <>
+      <Text>Hello</Text>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
